1cc726bab182447a2e102707b12eefe325cc6c7d
Yorcee <oluwatoyosipopoola2106@gmail.com>
2023-03-27 13:43:16 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,17 @@
 #!/bin/bash
-puts("Programming is like building a multilingual puzzle"\n"
+/* program to print a string using puts() */
+
+#include <stdio.h>
+
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line, using puts.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
