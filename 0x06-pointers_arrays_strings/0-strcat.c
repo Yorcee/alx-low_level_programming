06982ff32006a450fa97b3955c53e91b5cf6e246
Yorcee <oluwatoyosipopoola2106@gmail.com>
2023-03-29 13:52:50 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,10 @@
-* Return: void
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ *
+ * Return: void
  */
 char *_strcat(char *dest, char *src)
 {
